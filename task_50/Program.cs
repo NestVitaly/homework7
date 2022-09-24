3231882e78f683431a9673a5be64568b01fe36a2
NVitaly <whiskalik@gmail.com>
2022-09-24 14:53:59 +0300

    Задача 50, убрано лишнее

--- task_50/Program.cs
+++ task_50/Program.cs
@@ -7,33 +7,6 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-// Console.Write("Введите строку: ");
-// int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
-// Console.Write("Введите столбец: ");
-// int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-// int n = 5;
-// int m = 7;
-// Random random = new Random();
-// int[,] arr = new int[n, m];
-// Console.WriteLine("Исходный массив: ");
-// for (int i = 0; i < arr.GetLength(0); i++)
-// {
-//     for (int j = 0; j < arr.GetLength(1); j++)
-//     {
-//         arr[i, j] = random.Next(10, 99);
-//         Console.Write(arr[i, j]);
-//     }
-//     Console.WriteLine();
-// }
-// if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
-// {
-//     Console.WriteLine("Элемент не существует  ");
-// }
-// else
-// {
-//     Console.WriteLine($"Значение элемента массива = {0}", arr[pos1, pos2]);
-// }
-
 Console.WriteLine("Введите количество строк в массиве: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов в массиве: ");
